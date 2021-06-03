35c86f94febe5079cb3d32f5aa8901eac42e2feb
Rafath Bin Zafar Auvee <rbza98@outlook.com>
2021-06-04 01:00:03 +0600

    Checking the functionality

--- server/index.js
+++ server/index.js
@@ -2,22 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://bmrc:bmrc123@temp0.xq7v6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 6800;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
